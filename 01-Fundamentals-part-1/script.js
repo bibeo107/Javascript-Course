5b6eddb20ce0f9c037ecdfc680eb1a8338f93bec
Tham <thamvu107@gmail.com>
2023-08-15 10:26:36 +0700

    clean code

--- 01-Fundamentals-part-1/script.js
+++ 01-Fundamentals-part-1/script.js
@@ -14,10 +14,23 @@ let language;
 console.log(typeof isIsland);
 console.log(typeof language);
 
-//
+// Lecture: let, const and var
 language = "Vietnamese";
 console.log("language is " + language);
+//--------------------------------------------------------
+const currentYear = new Date().getFullYear();
+const birthdayJonas = 1989;
+const birthdaySarah = 1986;
+const ageJonas = currentYear - birthdayJonas;
+const ageSarah = currentYear - birthdaySarah;
+const isFulAge = ageJonas >= 18;
 
+console.log("Jonas's age " + ageJonas);
+console.log("Sarah's age " + ageSarah);
+console.log(ageJonas < ageSarah);
+console.log( ageJonas >= 18);
+
+// Lecture: Basic Operators
 const halfPopulation = population * 1000000/2;
 console.log (halfPopulation + " people would live in each half.");
 
@@ -29,26 +42,14 @@ if(morePopulation)
     console.log("My country have more people than Finland");
 else console.log("My country haven't more people than Finland")
 
-//Lecture: let, const and var
-const currentYear = new Date().getFullYear();
-const birthdayJonas = 1989;
-const birthdaySarah = 1986;
-const ageJonas = currentYear - birthdayJonas;
-const ageSarah = currentYear - birthdaySarah;
-console.log("Jonas's age " + ageJonas);
-console.log("Sarah's age " + ageSarah);
-console.log(ageJonas < ageSarah);
-console.log( ageJonas >= 18);
-const isFulAge = ageJonas >= 18;
-
 // Challenge 1
 const massMark = 78;
 const heightMark = 1.69;
 const massJon = 92;
 const heightJon = 1.95;
 
-const BMIMark = massMark / (heightMark*heightMark);
-const BMIJohn = massJon / (heightJon*heightJon);
+const BMIMark = massMark / (heightMark * heightMark);
+const BMIJohn = massJon / (heightJon * heightJon);
 
 console.log("BMIMark " + BMIMark);
 console.log("BMIJohn " + BMIJohn);
