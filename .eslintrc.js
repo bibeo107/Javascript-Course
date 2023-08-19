28b37be21cb0429baef82a07a23239c49bf5424d
Tham <thamvu107@gmail.com>
2023-08-20 00:15:33 +0700

    update lint use airbnb-base

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,42 +1,27 @@
 module.exports = {
-  "env": {
-    "browser": true,
-    "commonjs": true,
-    "es2021": true
+  env: {
+    browser: true,
+    es2021: true,
   },
-  "extends": "eslint:recommended",
-  "overrides": [
+  extends: 'airbnb-base',
+  overrides: [
     {
-      "env": {
-        "node": true
+      env: {
+        node: true,
       },
-      "files": [
-        ".eslintrc.{js,cjs}"
+      files: [
+        '.eslintrc.{js,cjs}',
       ],
-      "parserOptions": {
-        "sourceType": "script"
-      }
-    }
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
   ],
-  "parserOptions": {
-    "ecmaVersion": "latest"
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
-  "rules": {
-    "indent": [
-      "error",
-      "tab"
-    ],
-    "linebreak-style": [
-      "error",
-      "unix"
-    ],
-    "quotes": [
-      "error",
-      "single"
-    ],
-    "semi": [
-      "error",
-      "always"
-    ]
-  }
-}
+  rules: {
+    'no-console': 'off',
+  },
+};
---
+++ src/01-Fundamentals-part-1/Extra/script.js
@@ -0,0 +1,14 @@
+// Variable declarations
+/* var
+*/
+//----------------------------------------------------------------
+let width;
+function setWidth() {
+  width = 100;
+  console.log(`width: ${width}`);
+}
+
+setWidth();
+console.log(`width ${width}`); // 100
+
+//----------------------------------------------------------------
