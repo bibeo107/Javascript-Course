e0fa015b7b2cd10fffa7971de260cd46aa9d9496
Tham <thamvu107@gmail.com>
2023-08-20 10:39:30 +0700

    update gitlab  to use airbnb eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,43 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: 'airbnb-base',
-  overrides: [
-    {
-      env: {
-        node: true,
-      },
-      files: [
-        '.eslintrc.{js,cjs}',
-      ],
-      parserOptions: {
-        sourceType: 'script',
-      },
+    env: {
+        browser: true,
+        es2021: true,
     },
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
-  rules: {
-    'no-console': 'off',
-  },
+    extends: 'airbnb-base',
+    overrides: [
+        {
+            env: {
+                node: true,
+            },
+            files: [
+                '.eslintrc.{js,cjs}',
+            ],
+            parserOptions: {
+                sourceType: 'script',
+            },
+        },
+    ],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    rules: {
+        indent: [
+            'error',
+            4,
+        ],
+        'linebreak-style': [
+            'error',
+            'unix',
+        ],
+        quotes: [
+            'error',
+            'single',
+        ],
+        semi: [
+            'error',
+            'always',
+        ],
+        'no-console': 'off',
+    },
 };
