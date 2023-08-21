aa9b4795ed0f1e62d25c619bf531c3c2bfbd1cda
Tham <thamvu107@gmail.com>
2023-08-21 11:45:47 +0700

    add prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,43 +1,48 @@
 module.exports = {
-    env: {
-        browser: true,
-        es2021: true,
+    "env": {
+        "browser": true,
+        "es2021": true
     },
-    extends: 'airbnb-base',
-    overrides: [
+    "extends": ["airbnb-base", "plugin:prettier/recommended"],
+    "plugins": ["prettier"],
+    "overrides": [
         {
-            env: {
-                node: true,
+            "env": {
+                "node": true
             },
-            files: [
-                '.eslintrc.{js,cjs}',
+            "files": [
+                ".eslintrc.{js,cjs}"
             ],
-            parserOptions: {
-                sourceType: 'script',
-            },
-        },
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
     ],
-    parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
     },
-    rules: {
-        indent: [
-            'error',
+    "rules": {
+        "indent": [
+            "error",
             4,
         ],
-        'linebreak-style': [
-            'error',
-            'unix',
+        "linebreak-style": [
+            "error",
+            "unix",
         ],
-        quotes: [
-            'error',
-            'single',
+        "quotes": [
+            "error",
+            "single",
         ],
-        semi: [
-            'error',
-            'always',
+        "semi": [
+            "error",
+            "always",
         ],
-        'no-console': 'off',
-    },
-};
+        "no-console": "off",
+        "no-unused-vars": 0,
+        "no-debugger": 0,
+        "no-use-before-define": 0,
+        "max-len": 0
+    }
+}
