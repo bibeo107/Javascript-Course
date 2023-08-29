ba86a3b42ef17e1d9184a94a2683678098852d46
Tham <thamvu107@gmail.com>
2023-08-29 12:20:48 +0700

    learn about  hoisting in Javascript

--- README.md
+++ README.md
@@ -1,33 +1,46 @@
-
 # Practice to learn JavaScript
+
 ["The Complete JavaScript Course 2023: From Zero to Expert!" course](https://www.udemy.com/course/the-complete-javascript-course/)
 
 ## Clone
+
 Clone this repo by run:
+
 ```sh
 $ git clone https://gitlab.com/thamvu107/javascript-course.git
 ```
 
 ## How to Run
+
 ### Run JS with HTML
+
 Open the HTML file in the browser.<br />
 Open the inspect window:
- + Windows: Ctrl + Shift + I
- + Mac: Cmd + Option + I <br />
+
+-   Windows: Ctrl + Shift + I
+-   Mac: Cmd + Option + I <br />
 
 Select the Console tab to view the Javascript output.
 
 ### Run JS with node
+
 #### Require:
+
 Installing node.<br />
+
 #### Run JS code
+
 Open Terminal. <br/>
 Type: node jsFileName.js<br/>
+
 ### Run JS Using Code Runner Extension
+
 #### Require:
+
 Installing node.<br />
 Install the Code Runner Extension in VS code.<br />
+
 #### Run JS code
+
 Open JavaScript Code in VSCode then run code.<br />
 Run code: CTRL+ALT+N to view output.<br />
-
---
+++ src/01-Fundamentals-part-1/Extra/script.js
@@ -0,0 +1,85 @@
+/* eslint-disable prefer-const */
+/* eslint-disable no-var */
+/* eslint-disable vars-on-top */
+/* eslint-disable no-undef */
+/* eslint-disable prettier/prettier */
+
+// 1. Hoisting in JavaScript allows you to access functions and variables before they have been created.
+/* ----------------------------------------------------------------*/
+
+console.log('1. Hoisting in JavaScript');
+// 1.1  Variale Hoisting
+console.log('1.2 Variable Hoisting ');
+console.log(bar); // undefined
+var bar = 1;
+
+try {
+    console.log(foo); // ReferenceError: Cannot access 'foo' before initialization
+    let foo = 2; // TDZ
+} catch (e) {
+    console.log(e.message);
+}
+try {
+    console.log(foobar); // ReferenceError: Cannot access 'foo' before initialization
+    let foobar = 3; // TDZ
+} catch (e) {
+    console.log(e.message);
+}
+
+//----------------------------------------------------------------
+// 1.2 Function Hoisting
+console.log('1.2 Hoisting function declarations');
+
+console.log(square(5)); // 25
+
+function square(n) {
+    return n * n;
+}
+
+// Note: Function hoisting don't work with function expressions and arrow functions.
+try {
+    console.log(add(3, 5));
+    var add = function(i, j) {
+        return i + j;
+    };
+} catch (e) {
+    console.log(e.message);
+}
+
+try {
+    console.log(addArrowFunc(3, 6));
+    var addArrowFunc = (i, j) => {
+        return i + j;
+    };
+
+} catch (e) {
+    console.log(e.message);
+}
+
+// 1.3 Class Hoisting
+console.log('1.3 Class declaration hoisting');
+
+/* try {
+  new MyClass(); // ReferenceError: Cannot access 'MyClass' before initialization
+  class MyClass {}
+} catch (e) {
+  console.log(e.message);
+} */
+
+try {
+    var Frodo = new Hobbit();
+    Frodo.height = 100;
+    Frodo.weight = 300;
+    console.log(Frodo); // Output: ReferenceError: Hobbit is not defined
+
+    class Hobbit {
+        constructor(height, weight) {
+            this.height = height;
+            this.weight = weight;
+        }
+    }
+} catch (e) {
+    console.log(e.message);
+}
+
+// Import Hoisting
