2e7152454ccf128141775bd9bada92f56844a36e
Tham <thamvu107@gmail.com>
2023-12-01 03:40:40 +0700

    restructure test files and update run test on CI

---
+++ jest.config.js
@@ -0,0 +1,5 @@
+export default {
+    collectCoverageFrom: ['src/**/*.js', '!**/node_modules/**'],
+    coverageReporters: ['html', 'text', 'text-summary', 'cobertura'],
+    testMatch: ['**/*.test.js']
+};
