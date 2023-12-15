52fc5892056f88dc4fd6fef023434594542c6077
Tham <thamvu107@gmail.com>
2023-12-15 16:01:51 +0700

    update to clean code

--- src/Guess-My-Number/guessSecretNumber.js
+++ src/Guess-My-Number/guessSecretNumber.js
@@ -1,63 +1,75 @@
-/* eslint-disable func-names */
-const errorMessage = '⛔️ No number!';
-const lostMessage = '💥 You lost the game! 💥';
-const higherMessage = `Too high!`;
-const lowerMessage = `Too low!`;
-const correctMessage = '🌟Correct Number! 🌟';
+const scoreElement = document.querySelector('.score');
+const highScoreElement = document.querySelector('.highScore');
+const numberElement = document.querySelector('.number');
+const guessElement = document.querySelector('.guess');
+const bodyElement = document.querySelector('body');
+const checkBtn = document.querySelector('.check');
+const againBtn = document.querySelector('.again');
+
+const messages = {
+    noNumber: `⛔️ No number!`,
+    tooHigh: `Too high!`,
+    tooLow: `Too low!`,
+    invalidGuess: `Please enter a number between 1 and 20.`,
+    correctNumber: `🌟 Correct Number! 🌟`,
+    loseGame: `💥 You lost the game! 💥`,
+    startGuessing: `Start guessing...`
+};
+
 let score = 20;
 let highScore = 0;
-
 const getRandomNumber = (min, max) => Math.trunc(Math.random() * (max - min) + min);
 let secretNumber = getRandomNumber(1, 20);
 
-const displayMessage = function (message) {
+const displayMessage = (message) => {
     document.querySelector('.message').textContent = message;
 };
-const showCorrectNumber = function () {
-    document.querySelector('.number').textContent = secretNumber;
-    document.querySelector('body').style.backgroundColor = '#FFD700';
-    document.querySelector('.number').style.width = '30rem';
+const showCorrectNumber = () => {
+    numberElement.textContent = secretNumber;
+    numberElement.style.width = '30rem';
+    bodyElement.style.backgroundColor = '#FFD700';
 };
 
-export const checkNumber = function () {
-    const guess = Number(document.querySelector('.guess').value);
-    if (!guess) {
-        displayMessage(errorMessage);
+export const checkNumber = () => {
+    const guess = Number(guessElement.value);
+    if (!guess || guess < 1 || guess > 20) {
+        displayMessage(messages.invalidGuess);
         return;
     }
+
     if (guess === secretNumber) {
-        displayMessage(correctMessage);
+        displayMessage(messages.correctNumber);
         showCorrectNumber();
+
         if (score > highScore) {
             highScore = score;
-            document.querySelector('.highscore').textContent = highScore;
+            highScoreElement.textContent = highScore;
         }
-        return;
-    }
-    if (guess !== secretNumber) {
+    } else if (guess !== secretNumber) {
         if (score > 1) {
-            displayMessage(guess > secretNumber ? higherMessage : lowerMessage);
+            displayMessage(guess > secretNumber ? messages.tooHigh : messages.tooLow);
             score -= 1;
-            document.querySelector('.score').textContent = score;
+            scoreElement.textContent = score;
         } else {
-            displayMessage(lostMessage);
-            document.querySelector('.score').textContent = 0;
+            displayMessage(messages.loseGame);
+            scoreElement.textContent = 0;
         }
     }
 };
 
-export const resetGame = function () {
+export const resetGame = () => {
     score = 20;
     secretNumber = getRandomNumber(1, 20);
 
-    displayMessage('Start guessing...');
-    document.querySelector('.score').textContent = score;
-    document.querySelector('.number').textContent = '?';
-    document.querySelector('.guess').value = '';
+    displayMessage(messages.startGuessing);
 
-    document.querySelector('body').style.backgroundColor = '#222';
-    document.querySelector('.number').style.width = '15rem';
+    scoreElement.textContent = score;
+    numberElement.textContent = '?';
+    guessElement.value = '';
+
+    bodyElement.style.backgroundColor = '#222';
+    numberElement.style.width = '15rem';
 };
 
-document.querySelector('.check').addEventListener('click', checkNumber);
-document.querySelector('.again').addEventListener('click', resetGame);
+checkBtn.addEventListener('click', checkNumber);
+againBtn.addEventListener('click', resetGame);
